f101a7d3490cda85e6f970b0067312041cbf12d5
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-10 13:01:04 +0300

    login/logout

--- project/project/urls.py
+++ project/project/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from re import template
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -24,4 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_views.register, name='user-register'),
+    path("login/", auth_views.LoginView.as_view(template_name="user/login.html"), name='user-login'),
+    path("logout/", auth_views.LogoutView.as_view(template_name="user/logout.html"), name='user-logout'),
 ]
