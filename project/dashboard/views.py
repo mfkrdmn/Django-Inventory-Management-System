775b5593fe56beba76740bb2708f46ba2e930af3
mfkrdmn <muhammetfatihkaraduman@gmail.com>
2022-08-11 09:59:39 +0300

    adding items by using forms.py

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from .models import Product, Order
+from .forms import ProductForm
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+# from .decorators import auth_users, allowed_users
+
 
 @login_required(login_url="user-login")
 def index(request):
@@ -16,12 +20,23 @@ def products(request):
 
     all_products = Product.objects.all()
 
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            product_name = form.cleaned_data.get('name')
+            messages.success(request, f'{product_name} has been added')
+            return redirect('dashboard-products')
+    else:
+        form = ProductForm()
+
     context = {
-        "all_products" : all_products
+        "all_products" : all_products,
+        "form" :form,
     }
 
     return render(request, "dashboard/products.html",context)
-    
+
 @login_required(login_url="user-login")
 def order(request):
     return render(request, "dashboard/order.html")
